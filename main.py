130d7986875f18ac6e303b207f75f861be9e933b
brackman <brackman@gmail.com>
2015-06-14 23:24:14 -0700

    Updated how content is stored.  Pickle is failing to save without moving the namedtuple outside the class.  Need to change to JSON most likely -- something to do later.
    The initial data was collected as a dict() --need to go back and ensure it uses the class to start.

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import urllib.error
 from bs4 import BeautifulSoup
 import concurrent.futures
 
-
+grind = collections.namedtuple('grind', 'date, start, end, time')
 class GrouseMountainAccount:
+
     __last_update = 0
 
     def __init__(self, uuid=-1, name=None, age=None, dates_and_times=None):
@@ -39,8 +40,6 @@ class GrouseMountainAccount:
                     # <a href="/grind_stats/22597005000.json?page=2" data-remote="true">Last &raquo;</a>
                     # </span>
 
-                    grind = collections.namedtuple('grind', 'date, start, end, time')
-
                     rows = soup.find_all('tr')
                     for row in rows:
                         data = row.find_all(text=re.compile('\d.*'))
@@ -56,7 +55,7 @@ class GrouseMountainAccount:
                     if element is not None:
                         results = re.match(pattern, element['href'])
                         if results is not None:
-                            print("Result: Pages={}".format(results.groups()[0]))
+                            # print("Result: Pages={}".format(results.groups()[0]))
                             pages = int(results.groups()[0])
 
                     if page < pages:
@@ -69,7 +68,7 @@ class GrouseMountainAccount:
 
 
 def does_account_exist(number):
-    accounts = load_accounts()
+    accounts = load_data()
     root_url = "http://www.grousemountain.com/grind_stats/{}/".format(number)  # key2"
     if number not in accounts.keys():
         try:
@@ -92,25 +91,23 @@ def does_account_exist(number):
         pass
 
 
-def load_accounts():
-    _pickle_path = os.path.expanduser("~/Documents/grousemountain.pickle")
-
+def load_data(_storage_path=os.path.expanduser("~/Documents/grousemountain.pickle")):
+    print("[LOADING] {}".format(_storage_path))
     accounts = list()  # will load a list of GrouseMountainAccounts
-    if os.path.isfile(_pickle_path):
-        accounts = pickle.load(open(_pickle_path, 'rb'))
+    if os.path.isfile(_storage_path):
+        accounts = pickle.load(open(_storage_path, 'rb'))
 
     return accounts
 
 
-def save_accounts(accounts):
-    _pickle_path = os.path.expanduser("~/Documents/grousemountain.pickle")
-    pickle.dump(accounts, open(_pickle_path, 'wb'))
-
-    print("[SAVED] Grouse Mountain Accounts data saved with a total of {} entries.".format(len(accounts)))
+def save_data(data, _storage_path=os.path.expanduser("~/Documents/grousemountain.pickle")):
+    print("[SAVING] {}".format(_storage_path))
+    pickle.dump(data, open(_storage_path, 'wb'))
+    # print("[SAVED] Stored {} {} entries.".format(len(data), type(data[0])))
 
 
 def collect_account_numbers(pickle_path):
-    accounts = load_accounts()
+    accounts = load_data()
     pool = 120
     with concurrent.futures.ProcessPoolExecutor(pool) as executor:
         # accounts seem to end in:
@@ -133,18 +130,26 @@ def collect_account_numbers(pickle_path):
                 if r is not None:
                     accounts[r[0]] = r[1]
         # will incrementally save after each iteration through options
-        save_accounts(accounts)
+        save_data(accounts)
 
 
 if __name__ == "__main__":
     # collect_account_numbers(_file)
     # data = get_grind_times(12345)
-    accounts = load_accounts()
+
+    new_storage_path = os.path.expanduser("~/Documents/grousemountaindata.pickle")
+
+    accounts = load_data(_storage_path=new_storage_path)
+
+    new_accounts = list()
     for num, name in accounts.items():
-        if "Not Found" not in name:
-            #print("{}: {}".format(num, name))
-            test = GrouseMountainAccount(uuid=num)
-            test.get_grind_times()
-            for itm in test.dates_and_times:
-                print(itm)
-            1/0
+
+        account = GrouseMountainAccount(uuid=num)
+        account.name = name
+        if "Not Found" not in name and "Service Unavailable" not in name:
+            print("[info] Getting Grind times for: {}".format(account.name))
+            account.get_grind_times()
+        new_accounts.append(account)
+
+    save_data(new_accounts, _storage_path=new_storage_path)
+    print("Done!")
